275e47e1d1ea05443853285d2ecdb256cdd0c6bc
talgat <15talgat@mail.ru>
2022-12-24 16:51:22 +0300

    done zadacha_65

---
+++ zadacha_65/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 65: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+// M = 1; N = 5 -> "1, 2, 3, 4, 5"
+// M = 4; N = 8 -> "4, 6, 7, 8"
+
+
+void Recursion(int leftNumber, int rightNumber)
+{
+    if (rightNumber >= leftNumber)
+    {
+        Recursion(leftNumber, rightNumber - 1);
+        Console.Write(rightNumber + " ");
+    }
+}
+
+
+Console.Write("Введите левую границу: ");
+int leftNumber = int.Parse(Console.ReadLine());
+Console.Write("Введите правую границу: ");
+int rightNumber = int.Parse(Console.ReadLine());
+Recursion(leftNumber,rightNumber);
