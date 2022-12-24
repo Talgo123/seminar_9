4162d3078357adedb683b42595066148ed498325
talgat <15talgat@mail.ru>
2022-12-24 16:51:02 +0300

    done zadacha_63

---
+++ zadacha_63/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 63.
+// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N.
+
+void Recursion(int number)
+{
+    if (number >= 1)
+    {
+        Recursion(number - 1);
+        Console.Write(number + " ");
+    }
+}
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+Recursion(number);
