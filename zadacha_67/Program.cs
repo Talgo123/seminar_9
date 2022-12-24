a9fc2d73d41d003b1badefa58383a0cd05d74dfc
talgat <15talgat@mail.ru>
2022-12-24 16:51:51 +0300

    zadacha_67

---
+++ zadacha_67/Program.cs
@@ -0,0 +1,30 @@
+﻿// Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+int SumDigit(int number, int sum)
+{
+    sum += number %10;
+    if (number > 0)
+    {
+        SumDigit(number/10, sum);
+    }
+    else
+    {
+        System.Console.WriteLine(sum);
+    }
+    return sum;
+}
+
+int sum = 0;
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+sum = SumDigit(number, sum);
+
+
+// Другой вариант 
+// if (number < 0)
+// {
+//     return num;
+// }
+// return SumNumber(num / 10) + num % 10;
